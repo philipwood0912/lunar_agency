869c34d992ddcd90bb9ae40fe91f11667febc71d
Philip <philipwood0912@gmail.com>
2019-09-19 02:11:04 -0400

    fixed nav

--- js/main.js
+++ js/main.js
@@ -9,4 +9,15 @@ import mike from './modules/objectM.js';
     console.log(phil);
     console.log(brisk);
     console.log(mike);
+
+    var burgerCon = document.querySelector("#burgerCon"),
+        menuBut = document.querySelector("#menuCon");
+
+    function hamburgerMenu(){
+        console.log("button clicked");
+        burgerCon.classList.toggle("slideToggle");
+        menuBut.classList.toggle("expanded");
+    }
+
+    menuBut.addEventListener("click", hamburgerMenu);
 })();
